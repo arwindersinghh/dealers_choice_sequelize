c91804930526e89718b9da2a242a04c8d4e8025c
Arwinder Singh <arwindersinghh@gmail.com>
2020-11-19 03:12:36 -0500

    almost done

--- index.js
+++ index.js
@@ -1,16 +1,66 @@
 const Sequelize = require('sequelize');
 const { STRING } = Sequelize;
 
-const conn = new Sequelize('postgress://localhost/student_sport_db');
+const conn = new Sequelize('postgres://localhost/student_sport_db');
+
+const Student = conn.define('students', {
+    name: {
+        type: STRING
+    }
+});
+const Sportsteam = conn.define('sports_team', {
+    team: {
+        type: STRING
+    }
+
+});
+
+Student.belongsTo(Sportsteam, { as: 'teammate' });
+Sportsteam.hasMany(Student, { foreignKey: 'teammateId' });
+Student.belongsTo(Student, { as : 'captain' });
+Student.hasMany(Student, { foreignKey: 'captainId' });
 
 
 
 
 const syncAndSeed = async() => {
-    try{
-        await conn.sync({ force: true });
-    }
-    catch(ex){
-        console.log(ex)
+     await conn.sync({ force: true });
+     const [ Nathan, Oscar, Kevin, Daniel, Ginni, Kawai, Joseph ] = await Promise.all([
+         'Nathan', 'Oscar', 'Kevin', 'Daniel', 'Ginni', 'Kawai', 'Joseph'
+     ].map( name => 
+         Student.create({name})
+     ));
+     const studentArr = [ Nathan, Oscar, Kevin, Daniel, Ginni, Kawai, Joseph ];
+     const [ Basketball, Football, Soccer, Baseball ] = await Promise.all([
+        'Basketball', 'Football', 'Soccer', 'Baseball'
+    ].map( name => 
+        Sportsteam.create({team:name})
+    ));
+    Kevin.teammateId = Football.id;
+    Nathan.teammateId = Basketball.id;
+    Oscar.teammateId = Basketball.id;
+    Daniel.teammateId = Soccer.id;
+    Kawai.teammateId = Basketball.id;
+    Joseph.teammateId = Baseball.id;
+    
+    await Promise.all(
+        studentArr.map( student => student.save() ));
+
+        Nathan.captainId = Kawai.id;
+        Oscar.captainId = Kawai.id;
+        await Promise.all([
+            Nathan.save(),
+            Oscar.save()
+        ]);
+};
+
+
+
+module.exports = {
+    syncAndSeed,
+    conn,
+    models: {
+        Student,
+        Sportsteam
     }
 }
--- server.js
+++ server.js
@@ -1,15 +1,91 @@
-const Sequelize = require('sequelize');
-const { STRING } = Sequelize;
+const { syncAndSeed, conn, models: { Student, Sportsteam } } = require('./index');
+const express = require('express');
+const path = require('path');
+const app = express();
 
-const conn = new Sequelize('postgress://localhost/student_sport_db')
+app.use(express.static('assets'));
+
+app.get('/', async(req, res, next) => {
+    try{
+        res.send(`
+        <html>
+        <head>
+        <link rel='stylesheet' href='/styles.css'>
+        <title>Students and Teams</title>
+        </head>
+        <body>
+        <h1> Please pick one from below... </h1>
+        <div class='homepage'>
+        <li><a href='/sportsteams'> Sportsteams </a></li>
+        <li><a href='/students'> Students </a></li>
+        </div>
+        </body>
+        </html>
+        `)
+    }
+    catch(ex){
+        next(ex)
+    }
+});
 
 
+app.get('/sportsteams', async(req, res, next) => {
+    try{
+        res.send(await Sportsteam.findAll({
+            include : [
+                Student
+            ],
+            order: [
+                ['id', 'ASC']
+            ]
+        }))
+    }
+    catch(ex){
+        next(ex)
+    }
+})
+
+app.get('/students', async(req, res, next) => {
+    try{
+        res.send(await Student.findAll({
+            include : [
+                {
+                    model: Student,
+                    as: 'captain'
+                }
+               
+
+            ]
+        }));
+
+    }
+    catch(ex){
+        next(ex)
+    }
+});
 
 const init = async() => {
     try{
-        
+        await conn.authenticate();
+        await syncAndSeed();
+        const port = 3000;
+        app.listen(port, () => {
+            console.log(`You are being heard on port ${port}`);
+        });
     }
     catch(ex){
 
     }
 }
+init();
+
+
+
+
+
+
+
+
+
+
+
